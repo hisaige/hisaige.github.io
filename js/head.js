b231c1a14e2f230376a594aa3833e9e32940f77d
hisaige <hisaige@163.com>
2019-09-03 11:02:08 +0800

    添加js样式测试

--- js/head.js
+++ js/head.js
@@ -1,12 +1,16 @@
 ﻿document.write('<link type="text/css" rel="stylesheet" href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/monokai.min.css" />');
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/highlight.min.js"></script>');
+document.write('<script type="text/javascript" src="./highlight.min.js"></script>');
 
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/django.min.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/dockerfile.min.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/vim.min.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/yaml.min.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/gradle.min.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/tex.min.js"></script>');
+document.write('<script type="text/javascript" src="./django.min.js"></script>');
+document.write('<script type="text/javascript" src="./dockerfile.min.js"></script>');
+document.write('<script type="text/javascript" src="./vim.min.js"></script>');
+document.write('<script type="text/javascript" src="./yaml.min.js"></script>');
+document.write('<script type="text/javascript" src="./gradle.min.js"></script>');
+document.write('<script type="text/javascript" src="./tex.min.js"></script>');
+document.write('<script type="text/javascript" src="./shell.min.js"></script>');
+document.write('<script type="text/javascript" src="./php.min.js"></script>');
+document.write('<script type="text/javascript" src="./java.min.js"></script>');
+document.write('<script type="text/javascript" src="./xml.min.js"></script>');
 
 document.write('<script>hljs.initHighlightingOnLoad();</script>');
 
