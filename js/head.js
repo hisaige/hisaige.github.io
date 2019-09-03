b375272ebbb4d26f75ecfa546b92ac8cc4bdcfe6
hisaige <hisaige@163.com>
2019-09-03 10:39:50 +0800

    添加js样式测试

--- js/head.js
+++ js/head.js
@@ -1,11 +1,21 @@
-﻿<link href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/monokai.min.css" rel="stylesheet" />
-<script src="https://cdn.bootcss.com/highlight.js/9.12.0/highlight.min.js"></script>
+﻿document.write('<link type="text/css" rel="stylesheet" href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/monokai.min.css" />');
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/highlight.min.js"></script>');
 
-<script src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/django.min.js"></script>
-<script src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/dockerfile.min.js"></script>
-<script src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/vim.min.js"></script>
-<script src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/yaml.min.js"></script>
-<script src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/gradle.min.js"></script>
-<script src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/tex.min.js"></script>
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/django.min.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/dockerfile.min.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/vim.min.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/yaml.min.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/gradle.min.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/highlight.js/9.12.0/languages/tex.min.js"></script>');
 
-<script>hljs.initHighlightingOnLoad();</script>
+document.write('<script>hljs.initHighlightingOnLoad();</script>');
+
+
+
+
+
+
+
+
+
+
